0633c24e47560a080f4a1479901c9897fdca4506
LISOVSMA <116366290+LISOVSMA@users.noreply.github.com>
2023-04-01 23:09:54 +0300

    adds tasks 2-3

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,18 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+const STORAGE_TIME = 'videoplayer-current-time';
+
+const savedTime = localStorage.getItem(STORAGE_TIME);
+
+const getCurrentTime = function (e) {
+  const seconds = e.seconds;
+  localStorage.setItem(STORAGE_TIME, JSON.stringify(seconds));
+};
+
+player.on('timeupdate', throttle(getCurrentTime, 1000));
+
+player.setCurrentTime(JSON.parse(savedTime) || 0);
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,48 @@
+import throttle from 'lodash.throttle';
 
+const refs = {
+  form: document.querySelector('.feedback-form'),
+  email: document.querySelector('.feedback-form input'),
+  message: document.querySelector('.feedback-form textarea'),
+};
+
+const STORAGE_KEY = 'feedback-form-state';
+
+let formData = {};
+
+refs.form.addEventListener('submit', onFormSubmit);
+refs.form.addEventListener('input', throttle(onEmailInput, 500));
+
+fillTextarea();
+
+function onEmailInput(e) {
+  formData[e.target.name] = e.target.value;
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+}
+
+function onFormSubmit(e) {
+  e.preventDefault();
+
+  if (refs.email.value === '' || refs.message.value === '') {
+    return alert('Заповніть будь ласка всі поля');
+  }
+
+  console.log(formData);
+  e.currentTarget.reset();
+  localStorage.removeItem(STORAGE_KEY);
+}
+
+function fillTextarea() {
+  const savedData = localStorage.getItem(STORAGE_KEY);
+  const parseData = JSON.parse(savedData);
+
+  if (savedData) {
+    parseData.email === undefined
+      ? (refs.email.value = '')
+      : (refs.email.value = parseData.email);
+    parseData.message === undefined
+      ? (refs.message.value = '')
+      : (refs.message.value = parseData.message);
+    formData = parseData;
+  }
+}
